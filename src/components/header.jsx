a5b0e9c4748d4dcde5a50af5a1217128aab94931
eswar2001 <teswar2001@gmail.com>
2021-05-19 23:42:20 +0530

    added static projects data

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,6 +4,6 @@ export const Header = () => {
         <a href="mailto:eswar.abisheak94@gmail.com" title="Contact Me"
             rel='author'>Contact Me</a>
         <a href="https://docs.google.com/document/d/e/2PACX-1vSt0CHOS39X4-xG2cXxQHQxJfr89iIPN4b3sMXwE7fRmdOfAEnF_u1DPd-01pfO5NN9HbAlHU3wyDbv/pub" title="Linux based Website"
-            rel='author' target='_blank'>🌏</a>
+            rel="noreferrer" target='_blank'>🌏</a>
     </header>)
 }
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -4,39 +4,40 @@ import axios from "axios";
 export const Project = (props) => {
     const [list, setList] = useState([]);
     useEffect(() => {
-        axios.get(props.data.languages_url)
+        axios.get(props.__.languages_url)
             .then(response => {
+                console.log(response);
                 setList(Object.keys(response.data))
             })
     })
     var m = 99999;
-    if (props.data.homepage)
+    if (props.__.homepage)
         return (<div className='project' key={m++}>
-            <p style={{ fontWeight: 'bold' }} className="mt-1 ml-2"><a rel="noreferrer" href={props.data.homepage} title={props.data.name}>{props.data.name}</a></p>
+            <p style={{ fontWeight: 'bold' }} className="mt-1 ml-2"><a rel="noreferrer" href={props.__.homepage} title={props.__.name}>{props.__.name}</a></p>
             <p className="mt-1 ml-2">
-                {props.data.description}<br />
+                {props.__.description}<br />
                 <strong>Languages Used:</strong> {list.map((j) => {
                     return (<span> {j} </span>)
                 })}<br />
-                <strong>Created at: </strong>{moment(props.data.created_at).format('LL')}<br />
-                <strong>Last Pushed: </strong>{moment(props.data.pushed_at).format('LL')}
+                <strong>Created at: </strong>{moment(props.__.created_at).format('LL')}<br />
+                <strong>Last Pushed: </strong>{moment(props.__.pushed_at).format('LL')}
             </p>
-            <a rel="noreferrer" href={props.data.html_url}
-                title={props.data.name} className='ico-visit ml-5 mb-1' target='_blank' >Gtihub</a>
-            <a rel="noreferrer" href={props.data.homepage}
-                title={props.data.name} className='ico-visit ml-5 mb-1' target='_blank' >Visit</a>
+            <a rel="noreferrer" href={props.__.html_url}
+                title={props.__.name} className='ico-visit ml-5 mb-1' target='_blank' >Gtihub</a>
+            <a rel="noreferrer" href={props.__.homepage}
+                title={props.__.name} className='ico-visit ml-5 mb-1' target='_blank' >Visit</a>
         </div>)
     else {
         return (<div className='project' key={m++}>
-            <p style={{ fontWeight: 'bold' }} className="mt-1 ml-2"><a rel="noreferrer" href={props.data.homepage} title={props.data.name}>{props.data.name}</a></p>
-            <p className="mt-1 ml-2">{props.data.description}<br />
+            <p style={{ fontWeight: 'bold' }} className="mt-1 ml-2"><a rel="noreferrer" href={props.__.html_url} title={props.__.name}>{props.__.name}</a></p>
+            <p className="mt-1 ml-2">{props.__.description}<br />
                 <strong> Languages Used:</strong> {list.map((j) => {
                     return (<span> {j} </span>)
                 })}<br />
-                <strong>Created at: </strong>{moment(props.data.created_at).format('LL')}<br />
-                <strong>Last Pushed: </strong>{moment(props.data.pushed_at).format('LL')}</p>
-            <a rel="noreferrer" href={props.data.html_url}
-                title={props.data.name} className='ico-visit ml-5 mb-1' target='_blank' >Gtihub</a>
+                <strong>Created at: </strong>{moment(props.__.created_at).format('LL')}<br />
+                <strong>Last Pushed: </strong>{moment(props.__.pushed_at).format('LL')}</p>
+            <a rel="noreferrer" href={props.__.html_url}
+                title={props.__.name} className='ico-visit ml-5 mb-1' target='_blank' >Gtihub</a>
         </div>)
     }
 }
