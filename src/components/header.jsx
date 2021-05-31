1cc9e6619c4c7258d5a5a8420b6178305ef9d023
eswar2001 <teswar2001@gmail.com>
2021-05-31 15:28:20 +0530

    updated projects

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ export const Header = () => {
     return (<header>
         <a href="mailto:eswar.abisheak94@gmail.com" title="Contact Me"
             rel='author'>Contact Me</a>
-        <a href="https://docs.google.com/document/d/e/2PACX-1vSt0CHOS39X4-xG2cXxQHQxJfr89iIPN4b3sMXwE7fRmdOfAEnF_u1DPd-01pfO5NN9HbAlHU3wyDbv/pub" title="Linux based Website"
+        <a href="https://docs.google.com/document/d/1Qx8xBd9yMQ7jNC0nJZHoHwPjOjxb7Hu16P0oD3Hp2G8/edit?usp=sharing" title="Website"
             rel="noreferrer" target='_blank'>🌏</a>
     </header>)
 }
